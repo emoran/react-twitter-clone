5cfdc732da807c9f8e0ec1c465577e3afea1b214
Edgar Moran <edgarm@twitter.com>
2022-01-16 12:05:08 -0500

    profile stykes

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import {View,Text,StyleSheet,Image} from 'react-native';
+import {View,Text,StyleSheet,Image,FlatList} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { EvilIcons } from '@expo/vector-icons'; 
 
+
+
+
+
 export default function ProfileScreen(){
-    return (
+
+    const DATA = [
+        {
+          id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+          title: 'First Item',
+        },
+        {
+          id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+          title: 'Second Item',
+        }, 
+        {
+          id: '58694a0f-3da1-471f-bd96-145571e29d72',
+          title: 'Third Item',
+        },
+      ];
+
+      const profileHeader = () => (
         <View style={styles.container}>
            <Image 
             style={styles.backgroundImage}
@@ -37,16 +57,56 @@ export default function ProfileScreen(){
 
             <View style={styles.locationContainer}>
                <EvilIcons name="location" size={24} color="gray"/>
-               <Text style={styles.TextGray}></Text>
+               <Text style={styles.TextGray}>New York, USA</Text>
             </View>
 
-            <View style={styles.locationContainer}>
-                <TouchableOpacity style={styles.linkItem} onPress={()=> Linking.openURL('httpsL//yucelmoran.com')}>
-                   <EvilIcons name="calendar" size={24} color="gray"/>
-                   <Text style={styles.textColor}>Join on March 2020</Text>
+            <View style={styles.linkContainer}>
+                <TouchableOpacity style={styles.linkItem} 
+                    onPress={()=> Linking.openURL('httpsL//yucelmoran.com')}>
+                   <EvilIcons name="link" size={24} color="gray"/>
+                   <Text style={styles.textColor}>yucelmoran.com</Text>
                 </TouchableOpacity>
+                <View style={[styles.linkItem,styles.ml4]}> 
+                <EvilIcons name="calendar" size={24} color="gray"/>
+                <Text style={styles.TextGray}>Joined March 2019</Text>
+                </View>
             </View>
+
+            <View style={styles.followContainer}>
+                <View style={styles.followItem}>
+                    <Text style={styles.followItemNumber} >509</Text>
+                    <Text style={styles.followItemLabel} >Following</Text>
+                </View>
+
+                <View style={[styles.followItem, styles.ml4]}>
+                    <Text style={styles.followItemNumber} >100</Text>
+                    <Text style={styles.followItemLabel} >Followers</Text>
+                </View>
+            </View>
+
+            <View style={styles.separator}></View>
+
+            
         </View>
+    );
+
+
+      const renderItem = ({item}) => (
+        <View style={{marginVertical:20}}>
+            
+            <Text >{item.title}</Text>
+        </View>
+    );
+
+    return (
+        <FlatList
+        style={styles.container}
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => <View style={styles.tweetSeparator}></View>}
+        ListHeaderComponent={profileHeader}
+    />
     );
 
   
@@ -112,8 +172,41 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         marginTop: 4
     },
+    linkContainer: {
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        marginTop: 4,
+    },
     ml4:{
         marginLeft:14
+    },
+    linkColor: {
+        color: '#1d9bf1'
+    },
+    linkItem :{
+        flexDirection: 'row'
+    },
+    followContainer:{
+        flexDirection: 'row',
+        paddingHorizontal:10,
+        paddingVertical: 12
+    },
+    followItem:{
+        flexDirection: 'row'
+    },
+    followItemNumber:{
+        fontWeight: 'bold'
+    },
+    followItemLabel:{
+        marginLeft: 4
+    },
+    separator:{
+        borderBottomWidth:1,
+        borderBottomColor:'#E5E7EB'
+    },
+    tweetSeparator:{
+        borderBottomWidth:1,
+        borderBottomColor: '#e5e7eb'
     },
 
 })
